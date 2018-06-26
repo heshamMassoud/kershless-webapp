0d9f113c425c3b402eabffd09ccd4638c10999b5
Hesham Massoud <heshamhamdymassoud@gmail.com>
2018-06-26 23:43:48 +0200

    Add files after Next.js/learn tutorial.

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+import Layout from '../components/MyLayout'
+
+export default () => (
+    <Layout>
+        <div> This is the about us!!</div>
+    </Layout>
+)
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,60 @@
-import { Component } from 'react'
-import data from './../data/person'
-import PersonList from '../components/PersonList'
+import Layout from '../components/MyLayout.js'
+import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
 
-const Index = () => (  
-  <div>
-      <PersonList data={data} />
-  </div>
+const ShowLink = ({ show }) => (
+  <li key={show.id}>
+    <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+      <a>{show.name}</a>
+    </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: "Arial";
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+  </li>
 )
+
+
+const Index = (props) => (
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(({ show }) => (
+        <ShowLink key={show.id} show={show} />
+      ))}
+    </ul>
+    <style jsx>{`
+      h1 {
+        font-family: "Arial";
+      }
+      ul {
+        padding: 0;
+      }
+    `}</style>
+  </Layout>
+)
+
+Index.getInitialProps = async function () {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+  const data = await res.json()
+
+  console.log(`Show data fetched. Count: ${data.length}`)
+
+  return {
+    shows: data
+  }
+}
 
 export default Index
